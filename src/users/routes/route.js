c8e9f6039c8177e5f4288d4bf52fc81a4d7ffb9b
programadorisgod <catapiascastro@gmail.com>
2024-11-04 11:26:34 -0500

    Refactor user routes and add new endpoints

---
+++ src/users/routes/route.js
@@ -0,0 +1,17 @@
+import {
+    createUser,
+    deleteUserByUsername,
+    login,
+    getUserByUsername,
+} from '../controller/controller.js'
+import { BASE_URL } from '../../CONSTANTS/constant.js'
+import { Router } from 'express'
+
+const router = Router()
+
+router.get(`${BASE_URL}/users/:username`, getUserByUsername)
+router.post(`${BASE_URL}/users/auth/login/:username`, login)
+router.post(`${BASE_URL}/users`, createUser)
+router.delete(`${BASE_URL}/users/:username`, deleteUserByUsername)
+
+export { router }
